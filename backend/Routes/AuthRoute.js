13bbb326a39ae383386188bac8cfd5add455919e
Luigi Michele Improda <limproda@gmail.com>
2025-04-10 22:14:40 -0500

    Backend - Login and Signup working

--- backend/Routes/AuthRoute.js
+++ backend/Routes/AuthRoute.js
@@ -1,8 +1,11 @@
-import { Signup } from "../Controllers/AuthController.js";
+import { Signup, Login } from "../Controllers/AuthController.js";
+import { userVerification } from "../Middlewares/AuthMiddleware.js";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/signup", Signup);
+router.post("/login", Login);
+router.post("/", userVerification);
 
 export default router;
