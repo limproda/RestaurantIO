7a6385d0c24a2e4dae7398463c0f4440d4c2f63e
Luigi Michele Improda <limproda@gmail.com>
2025-05-17 15:55:51 -0500

    Corregir URL api

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,5 +1,5 @@
 export const appName = "RestaurantIO";
 export const FRONTEND_URL = "http://localhost:5173";
-export const API_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/api" // Ruta dinámica para el backend
+export const API_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "" // Ruta dinámica para el backend
 export const TRANS_URL = `${API_URL}/transactions`
 export const sidebarWidth = 240;
